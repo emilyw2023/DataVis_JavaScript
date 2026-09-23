8edce7c7eb9d4f24422785ee3334e3f74dbf3702
emilyw2023 <emilyweir2023@gmail.com>
2026-09-22 23:12:58 -0400

    HTML

---
+++ main.js
@@ -0,0 +1,18 @@
+
+let numInt= 5
+let numFloat= 5.5
+
+let firstName= "Emily"
+let lastName= "Weir"
+
+let isdataloaded= false
+let passed= true
+let failed= !passed
+
+let nums= [1, 2, 3, 4, 5]
+let course= [ "Data Vis","Typography","Digital Platforms"]
+
+let myElement= document.getElementById("para")
+
+myElement.style.color="blue"
+myElement.style.fontSize= "2rem"
